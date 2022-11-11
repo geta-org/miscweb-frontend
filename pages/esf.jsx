b93f7eb3c844df21500c6fa92697494933c14c4b
pedroGNF <pedrognfwork@gmail.com>
2022-11-11 00:01:58 -0300

    Tela Ações, added

--- pages/esf.jsx
+++ pages/esf.jsx
@@ -1,29 +1,28 @@
-import Head from 'next/head'
-import Image from 'next/future/image'
-import Link from "next/link"
-import esfHome from '../public/TelaESF/esfTela.png'
+import Head from "next/head"
+import Image from "next/future/image"
+import acoesHome from "../public/TelaESF/esfTela.png"
 
 function Header({ title }) {
-    return <h1>{title ? title : 'ESF'}</h1>
+    return <h1>{title ? title : "ESF"}</h1>
 }
 
-const ftEsf = {
-    width: '50%',
-    height: 'auto',
-    position: 'absolute',
-    center: '5.0px',
-    border: '2px solid black',
-    borderRadius: '0% / 0%',
+const ftAcoes = {
+    width: "50%",
+    height: "auto",
+    position: "absolute",
+    center: "5.0px",
+    border: "2px solid black",
+    borderRadius: "0% / 0%",
 }
 
 export default function Esf() {
     return (
         <>
             <Head>
-                <Image src={esfHome} style={ftEsf} alt="Símbolo da ESF" />
+                <Image src={acoesHome} style={ftAcoes} alt="Símbolo da ESF" />
             </Head>
             <div>
-                <Header title="Estrateja de Saúde da Família" />
+                <Header title="Ações junto a a Comunidade" />
             </div>
         </>
     )
